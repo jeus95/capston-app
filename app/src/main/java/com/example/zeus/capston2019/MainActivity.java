32c45f7d64899665c1472c07a371965014706241
jeus95 <jjw9551@naver.com>
2019-04-30 18:13:44 +0900

    최초 커밋

--- app/src/main/java/com/example/zeus/capston2019/MainActivity.java
+++ app/src/main/java/com/example/zeus/capston2019/MainActivity.java
@@ -17,31 +17,73 @@ public class MainActivity extends AppCompatActivity {
         //    2. AndroidManifest.xml 에 Activity 를 등록한다
         //    3. Intent 객체를 만들어서 startActivity 한다
 
-        Button STPower = findViewById(R.id.button1);
-        Button CPower = findViewById(R.id.button2);
-        STPower.setOnClickListener(new View.OnClickListener() {
+        Button StPower_b = findViewById(R.id.button1);
+        Button CPower_b = findViewById(R.id.button2);
+        Button Schedule_b = findViewById(R.id.button3);
+        Button CalPower_b = findViewById(R.id.button4);
+        Button Voice_b = findViewById(R.id.button5);
+
+        StPower_b.setOnClickListener(new View.OnClickListener() {
+                                         @Override
+                                         public void onClick(View view) {
+                                             Intent intent = new Intent(
+                                                     getApplicationContext(), // 현재 화면의 제어권자
+                                                     StPower.class); // 다음 넘어갈 클래스 지정
+                                             startActivity(intent); // 다음 화면으로 넘어간다
+                                         }
+                                     }
+
+
+        );
+        CPower_b.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View view) {
+                                            Intent intent = new Intent(
+                                                    getApplicationContext(), // 현재 화면의 제어권자
+                                                    CPower.class); // 다음 넘어갈 클래스 지정
+                                            startActivity(intent); // 다음 화면으로 넘어간다
+                                        }
+                                    }
+
+
+        );
+        Schedule_b.setOnClickListener(new View.OnClickListener() {
+                                          @Override
+                                          public void onClick(View view) {
+                                              Intent intent = new Intent(
+                                                      getApplicationContext(), // 현재 화면의 제어권자
+                                                      Schedule.class); // 다음 넘어갈 클래스 지정
+                                              startActivity(intent); // 다음 화면으로 넘어간다
+                                          }
+                                      }
+
+
+        );
+        CalPower_b.setOnClickListener(new View.OnClickListener() {
+                                          @Override
+                                          public void onClick(View view) {
+                                              Intent intent = new Intent(
+                                                      getApplicationContext(), // 현재 화면의 제어권자
+                                                      Calpower.class); // 다음 넘어갈 클래스 지정
+                                              startActivity(intent); // 다음 화면으로 넘어간다
+                                          }
+                                      }
+
+
+        );
+        Voice_b.setOnClickListener(new View.OnClickListener() {
                                        @Override
                                        public void onClick(View view) {
                                            Intent intent = new Intent(
                                                    getApplicationContext(), // 현재 화면의 제어권자
-                                                   MyTwo.class); // 다음 넘어갈 클래스 지정
+                                                   Voice.class); // 다음 넘어갈 클래스 지정
                                            startActivity(intent); // 다음 화면으로 넘어간다
                                        }
                                    }
 
 
         );
-        CPower.setOnClickListener(new View.OnClickListener() {
-                                      @Override
-                                      public void onClick(View view) {
-                                          Intent intent = new Intent(
-                                                  getApplicationContext(), // 현재 화면의 제어권자
-                                                  MyThree.class); // 다음 넘어갈 클래스 지정
-                                          startActivity(intent); // 다음 화면으로 넘어간다
-                                      }
-                                  }
 
 
-        );
     } // end onCreate()
 } // end MainActivity
